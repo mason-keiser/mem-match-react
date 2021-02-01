861fc46698967f5f8a99d60c04f76a2740c32696
Mason Keiser <masonksr5@gmail.com>
2021-02-01 05:50:48 +0000

    home page intact 90%

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,9 +1,34 @@
+import { useState } from "react"
 import React from 'react'
+import Home from './landing'
 
 const App = (props) => {
+
+    const [view, setView] = 
+        useState({view: {
+            name: 'init',
+            params: {}
+        }})
+    
+    const newView = (names, params) => {
+        setView({
+            view: {
+                name: names,
+                params: params
+            }
+        })
+    }
+
+    let tert = (view.view.name === 'init')
+        ?  <div>Workin</div>
+        : (view.view.name === 'old') 
+          ? <div>old Work</div>
+          : null 
+
     return (
         <div className='appCont'>
-            working
+            <Home/>
+            
         </div>
     )
 }
---
+++ client/components/landing.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Home = (props) => {
+    return (
+        <div className='home_bg'>
+            <img src="/images/uw.jpg" alt="homeScreen" id='home'/>
+            <div className="loginButton" > Login </div>
+            <div className="title">
+                <h1>Under The Sea</h1>
+                <h3>Memory Match Game</h3>
+            </div>
+        </div>
+    )
+}
+
+export default Home
