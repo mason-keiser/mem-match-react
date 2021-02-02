49c614d38e66ad3a6cf7dfac0d8640682b0840b4
Mason Keiser <masonksr5@gmail.com>
2021-02-02 07:06:43 +0000

    login fe created

--- client/components/login.jsx
+++ client/components/login.jsx
@@ -4,9 +4,30 @@ import Navb from './navbar';
 const Login = (props) => {
 
     return (
-        <div style={{height: '100vh'}}>
-            <img src="/images/uw.jpg" alt="homeScreen" id='home'/>
+        <div className='con'>
+            <img src="/images/uw.jpg" alt="homeScreen" id='homeL'/>
             <Navb props = {props}/>
+            <div className='loginCont'>
+                <h1>Login</h1>
+                <div>
+                    <form className='loginForm'>
+                        <div className='form-group'>
+                            <label htmlFor="name">Name: </label>
+                            <input type="text" autoComplete='name' placeholder='Name' name='name' className="" id='name'/>
+                        </div>
+                        <div className='form-group'>
+                            <label htmlFor="password">Password: </label>
+                            <input type="password" placeholder='Password' autoComplete='password' name='password' className="" id='password'/>
+                        </div>
+                        <div id="required" className="required"></div>
+                        <div className='loginbutton'>
+                            <button type='submit' className='btn mt-2 white' style={{color: 'white'}}>Login</button>
+                        </div>
+                        <h4 className='needacc'>Click here to sign up</h4>
+                    </form>
+
+                </div>
+            </div>
         </div>
     )
 }
