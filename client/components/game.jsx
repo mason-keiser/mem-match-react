cd5b59a9164846cbe2cc5c71e1c847972f690b13
Mason Keiser <masonksr5@gmail.com>
2021-02-09 00:32:55 +0000

    hard coded gameInfo row

--- client/components/game.jsx
+++ client/components/game.jsx
@@ -1,13 +1,44 @@
 import React,{useState, useEffect, useReducer} from 'react';
 import Navb from './navbar';
+import {
+    Container,
+    Row,
+    Col
+  } from 'reactstrap';
 
 const Game = (props) => {
     return (
         <div>
-            <div style={{position: 'absolute', zIndex: '999', width: '100vw'}}>
+            <div style={{zIndex: '999', width: '100vw'}}>
                 <Navb props={props}/>
             </div>
-            <img src="/images/uw.jpg" alt="homeScreen" id='homeL'/>
+            <img src="/images/uw.jpg" alt="homeScreen" id='homeL' style={{position: 'absolute', zIndex: '-1', top: 0}}/>
+            <Container id='gameInfo' className='d-flex justify-content-center align-content-center align-items-center'>
+                <Row>
+                    <Col xs="12">
+                        <Row className=" fade-in row-cols-1 row-cols-md-3 row-cols-lg-3 justify-content-center pt-5">
+                            <Col className="mb-2" id='fold'>
+                                <div id='infoI'>
+                                    <h2>Total Matches: </h2>
+                                    <h3>3</h3>
+                                </div>
+                            </Col>
+                            <Col className="mb-2" id='fold'>
+                                <div id='infoI'>
+                                    <h2>Accuracy: </h2>
+                                    <h3>58.97%</h3>
+                                </div>
+                            </Col>
+                            <Col className="mb-2" id='fold'>
+                                <div id='infoI'>
+                                    <h2>Games Played: </h2>
+                                    <h3>69</h3>
+                                </div>
+                            </Col>
+                        </Row>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     )
 }
