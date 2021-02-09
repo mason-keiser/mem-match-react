ab54287921d5a9293dbac00e1d63aaea1294058f
Mason Keiser <masonksr5@gmail.com>
2021-02-09 03:30:23 +0000

    added both sides of card

--- client/components/game_card.jsx
+++ client/components/game_card.jsx
@@ -13,6 +13,7 @@ const Game_Card = (props) => {
     const handleClick = (event) => {
         const back = document.querySelectorAll('.backCard');
         const front = document.querySelectorAll('.frontCard');
+        
         back.forEach(b => {
           if (b.id === event.target.id) {
             console.log(b)
@@ -24,22 +25,31 @@ const Game_Card = (props) => {
   return (
     <Col 
       className="mr-5 ml-5 mobile-card m-auto skillC p-0" id='skillC' style={{width: 'fit-content'}}>
-      <Card onClick={(e) => handleClick(e)} data-tilt className='gameCard js-tilt skills-card border-0 slide-in-10 p-0' id={props.card.name}>
-        <CardBody className='m-auto' style={{flex: 'none'}} id={props.card.name}>
         <div>
             <div id={props.card.name} className='frontCard'>
-                
+              <Card onClick={(e) => handleClick(e)} data-tilt className='gameCard js-tilt skills-card border-0 slide-in-10 p-0 c' style={{background: 'rgba(0,0,0, 0.5)'}} id={props.card.name}>
+                <CardBody className='m-auto' style={{flex: 'none'}} id={props.card.name}>
+                <div className="icon3 decoration-none border-div-small d-flex" id={props.card.name}>
+                    <h2 src={props.card.image}
+                        className="iconName border-rounded proj-img"
+                        id={props.card.name}> UTS</h2>
+                </div>
+                </CardBody>
+              </Card>
+              
             </div>
             <div id={props.card.name} className='backCard flex-column justify-content-center'>
+              <Card onClick={(e) => handleClick(e)} data-tilt className='gameCard js-tilt skills-card border-0 slide-in-10 p-0 c' style={{background: 'rgba(255,255,255, 0.5)'}} id={props.card.name}>
+                <CardBody className='m-auto' style={{flex: 'none'}} id={props.card.name}>
                 <div className="icon2 decoration-none border-div-small d-flex" id={props.card.name}>
                     <img src={props.card.image}
                         className="cardImg border-rounded proj-img"
                         id={props.card.name} />
                 </div>
+                </CardBody>
+              </Card>
             </div>
         </div>
-        </CardBody>
-      </Card>
     </Col>
     )
 }
