995d4642e78efd16ddfdd25cb462f3385d873fe6
Mason Keiser <masonksr5@gmail.com>
2021-02-19 00:35:07 +0000

    icons styled

--- client/components/userIcons.jsx
+++ client/components/userIcons.jsx
@@ -33,10 +33,47 @@ const UserIcons = (props) => {
     }
 
     return (
-        <div id='gameBack' style={{height: '100vh'}}>
+        <div className='userIconback'>
              <div style={{zIndex: '999', width: '100vw'}}>
                 <Navb props={props}/>
             </div>
+            <div className ='d-flex flex-column align-items-center justify-content-center' id='ce'>
+                <h1 className='iconTitle'>Change User Icon</h1>
+                <h4 className='iconSubTitle'>Icon Selection: </h4>
+            </div>
+            <Container id='iconContainer' style={{width: '100vw'}} className='d-flex justify-content-center align-content-center align-items-center'>
+                <Row>
+                    <Col xs="12">
+                        <Row className="d-flex m-auto fade-in row-cols-3 row-cols-md-2 row-cols-lg-3 justify-content-center pt-5 pb-5">
+                            <Col>
+                                <div className='iconHolder'>
+                                    <img src="/images/userIcons/dolphinIcon.png" alt="dolphin"/>
+                                </div>
+                            </Col>
+                            <Col>
+                                <div className='iconHolder'>
+                                    <img src="/images/userIcons/jellyfishIcon.png" alt="jellyfish" />
+                                </div>
+                            </Col>
+                            <Col>
+                                <div className='iconHolder'>
+                                    <img src="/images/userIcons/fishIcon.png" alt="fish"/>
+                                </div>
+                            </Col>
+                            <Col>
+                                <div className='iconHolder' id='turtle'>
+                                    <img src="/images/userIcons/turtleIcon.png" alt="turtle"/>
+                                </div>
+                            </Col>
+                            <Col>
+                                <div className='iconHolder'>
+                                    <img src="/images/userIcons/whaleIcon.png" alt="whale"/>
+                                </div>
+                            </Col>
+                        </Row>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     )
 }
